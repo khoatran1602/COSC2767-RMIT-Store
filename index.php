9f8553a7264e1bd629e2a1ef0aee8e52e3395894
Tran Dang Khoa <trankhoa758@gmail.com>
2024-01-03 23:10:38 +0700

    Update database connection IP address in index.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
-                  "44.219.102.6",
+                  "44.219.102.6", // Mariadb IP
                   "db_admin",
                   "rmit_password",
                   "rmit_store_db"
