deef9a71454dabe472a8c3b18d02beefd01fdc93
Tran Dang Khoa <trankhoa758@gmail.com>
2024-01-21 00:17:48 +0700

    Update store title and fix indentation in DatabaseConnectionTest

--- tests/DatabaseConnectionTest.php
+++ tests/DatabaseConnectionTest.php
@@ -3,17 +3,17 @@ use PHPUnit\Framework\TestCase;
 
 class DatabaseConnectionTest extends TestCase {
     public function testDatabaseConnection() {
-   $link = mysqli_connect(
-     "44.195.95.46:8081",
-     "db_admin",
-     "rmit_password",
-     "rmit_store_db"
-   );
+        $link = mysqli_connect(
+            "44.195.95.46:8081",
+            "db_admin",
+            "rmit_password",
+            "rmit_store_db"
+        );
 
 
 
-   $this->assertInstanceOf(mysqli::class, $link);
-   // Checks if $link is an instance of the mysqli class,
-   // which implies a successful database connection.
+        $this->assertInstanceOf(mysqli::class, $link);
+        // Checks if $link is an instance of the mysqli class,
+        // which implies a successful database connection.
     }
 }
