504a3c88c48bd1d39a100fbdb7be223fb77faf95
Tran Dang Khoa <trankhoa758@gmail.com>
2024-01-22 13:35:11 +0700

    Update server IP address in DatabaseConnectionTest.php

--- tests/DatabaseConnectionTest.php
+++ tests/DatabaseConnectionTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 class DatabaseConnectionTest extends TestCase {
     public function testDatabaseConnection() {
         $link = mysqli_connect(
-            "44.195.95.45:8081", #Change this to your server IP address
+            "44.195.95.44:8081", #Change this to your server IP address
             "db_admin",
             "rmit_password",
             "rmit_store_db"
